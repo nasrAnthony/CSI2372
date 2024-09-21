8aebb2018fc45a0710bb99e3e4c9955683046333
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-09-21 16:00:07 -0400

    formatting

--- Lab-Assignment1/myfile2.h
+++ Lab-Assignment1/myfile2.h
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <iomanip>
 #include <process.h>
+
 using namespace std;
+
 const double Pi = 3.14159;
+
 char menu(void);
 int volume(double const& ray, double const& height);
 int surface(double const& ray);
