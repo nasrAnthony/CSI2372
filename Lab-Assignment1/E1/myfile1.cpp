954965976e6a39ad79e2b924062134bcb8599c66
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-09-21 23:26:58 -0400

    Changed to using header file for E1

--- Lab-Assignment1/E1/myfile1.cpp
+++ Lab-Assignment1/E1/myfile1.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include <iomanip>
+#include "myfile1.h"
 
 using namespace std;
+
+void variableInfo(){
+    cout << "\nSize in bytes of a character: " << sizeof(char) << endl;
+    cout << "Size in bytes of a Integer: " << sizeof(int) << endl;
+    cout << "Size in bytes of a float: " << sizeof(float) << endl;
+    cout << "Size in bytes of a double: " << sizeof(double) << endl;
+    cout << "Size in bytes of a short Integer: " << sizeof(short int) << endl;
+    cout << "Size in bytes of an unsigned integer: " << sizeof(unsigned int)  << "\n" << endl;
+}
+
+void convertInteger(int num){
+    //print results:
+    cout << "\nnumber in decimal \t" << std::dec << num << endl;
+    cout << "number in octal   \t" << std::oct << num << endl;
+    cout << "number in hexa    \t" << std::hex << num << "\n" << endl;
+}
+
+void convertInteger2ndMethod(int num){
+    //the above code can be accomplished through usage of the printf() function...
+    printf("number in decimal\t%d\n", num); //indicate decimal formatting with %d
+    printf("number in octal  \t%o\n", num); //indicate octal formatting with %o
+    printf("number in hexa   \t%x\n", num); //indicate hexadecimal formatting with %x
+}
+
+void convertRealNumber(double realNum){
+    cout << fixed << setprecision(3);
+    cout << realNum << endl;
+    //cout << hexfloat << setprecision(13);
+    //cout << userInputRealNumber << endl;
+    printf("%.3a\n", realNum); //how to add 0s?
+}
+
+void convertHex(char inputChar){
+    cout << inputChar << endl;
+    cout << (int)inputChar << endl;
+}
 
 int main(){
 
@@ -9,49 +46,31 @@ int main(){
      * first portion
      * print the different data types and their respective sizes
     **/
-    cout << "\nSize in bytes of a character: " << sizeof(char) << endl;
-    cout << "Size in bytes of a Integer: " << sizeof(int) << endl;
-    cout << "Size in bytes of a float: " << sizeof(float) << endl;
-    cout << "Size in bytes of a double: " << sizeof(double) << endl;
-    cout << "Size in bytes of a short Integer: " << sizeof(short int) << endl;
-    cout << "Size in bytes of an unsigned integer: " << sizeof(unsigned int)  << "\n" << endl;
+    variableInfo();
 
     //second portion
     int userInput;
     cout  << "Enter an integer: ";
     cin  >> userInput;
-    
-    //print results:
-    cout << "\nnumber in decimal \t" << std::dec << userInput << endl;
-    cout << "number in octal   \t" << std::oct << userInput << endl;
-    cout << "number in hexa    \t" << std::hex << userInput << "\n" << endl;
-    
-    
+
+    convertInteger(userInput);
+
     //the above code can be accomplished through usage of the printf() function...
-    printf("number in decimal\t%d\n", userInput); //indicate decimal formatting with %d
-    printf("number in octal  \t%o\n", userInput); //indicate octal formatting with %o
-    printf("number in hexa   \t%x\n", userInput); //indicate hexadecimal formatting with %x
+    convertInteger2ndMethod(userInput);
     
     //Third portion of exercise 3
     double userInputRealNumber;
     cout  << "\nEnter a real number: ";
     cin  >> userInputRealNumber;
 
-    cout << fixed << setprecision(3);
-    cout << userInputRealNumber << endl;
-
-    //cout << hexfloat << setprecision(13);
-    //cout << userInputRealNumber << endl;
-    printf("%.3a\n", userInputRealNumber); //how to add 0s?
-
+    convertRealNumber(userInputRealNumber);
 
     //3rd part:
     char userInputChar;
     cout << "\nEnter a character: ";
     cin >> userInputChar;
 
-    cout << userInputChar << endl;
-    cout << (int)userInputChar << endl;
+    convertHex(userInputChar);
 
     return 0;
 }
---
+++ Lab-Assignment1/E1/myfile1.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void variableInfo();
+
+void convertInteger(int num);
+
+void convertInteger2ndMethod(int num);
+
+void convertRealNumber(double realNum);
+
+void convertHex(char inputChar);
