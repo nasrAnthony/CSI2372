63bfa46df2a2153c51a8d93a1cacd8bc6a8c4970
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-11-09 15:57:24 -0500

    Implemented Card and chain class

--- project/Card.h
+++ project/Card.h
@@ -3,4 +3,144 @@
 
 using namespace std;
 
-class Card;
+class Card{
+
+    public:
+        virtual int getCardsPerCoin (int coins) = 0;
+        virtual string getName() = 0;
+        virtual void print(ostream& out) = 0;
+        friend ostream& operator<< (ostream& out, Card& c){
+            c.print(out);
+            return out;
+        }
+};
+
+class Blue : public Card {
+    const string name;
+    int coinTable [4];
+    public:
+        Blue() : name("Blue"), coinTable{4,6,8,10}{}
+
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class Chili : public Card {
+    const string name;
+    int coinTable [4];
+    public:
+        Chili(): name("Chili"), coinTable{3,6,8,9}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class Stink : public Card {
+    const string name;
+    int coinTable [4];
+    public:
+        Stink(): name("Stink"), coinTable{3,5,7,8}{}
+        int getCardsPerCoin(int coins){
+            return 0;
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class Green : public Card {
+    const string name;
+    int coinTable[4];
+    public:
+        Green(): name("Green"), coinTable{3,5,6,7}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class soy : public Card {
+    const string name;
+    int coinTable[4];
+    public:
+        soy() : name("soy"), coinTable{2,4,6,7}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class black : public Card {
+    const string name;
+    int coinTable[4];
+    public:
+        black(): name("black"), coinTable{2,4,5,6}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class Red : public Card {
+    const string name;
+    int coinTable[4];
+    public:
+        Red(): name("Red"), coinTable{2,3,4,5}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1));
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
+
+class garden : public Card {
+    const string name;
+    int coinTable[4];
+    public:
+        garden(): name("garden"), coinTable {0,2,3,0}{}
+        int getCardsPerCoin(int coins){
+            return *(coinTable + (coins - 1)); //if 0 it means we cannot have that amount of coins with this card. 
+        }
+        string getName(){
+            return name;
+        }
+        void print(ostream& out){
+            out << name.front();
+        }
+};
--- project/Chain.h
+++ project/Chain.h
@@ -1,6 +1,99 @@
 #include <iostream>
+#include <vector>
+#include <exception>
 #include "Card.h"
+
 
 using namespace std;
 
-template <class T> class Chain;
+
+template <class T> class Chain{
+    vector<T*> cardChain;
+    public:
+        Chain(istream&); //constructor to build chain from file when game is resumed
+        Chain(Card * firstCard) { cardChain.push_back(firstCard);} //default constructor
+        Chain<T>& operator+=(Card*); //operator overload for (adding a card)
+        int sell();
+        friend ostream& operator << (ostream& out, Chain<Card>& c); //friend << operator
+};  
+
+template <class T> Chain<T>::Chain(istream&){
+    while(!in.eof()){
+        string input;
+        in >> input;
+        if(input.size() > 0){
+            if(input.at(0) == 'B'){ //card read from file
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new Blue();
+                    cardChain.push_back(tmpCard);
+                }
+            }
+            if(input.at(0) == 'C'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new Chili();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 'S'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new Stink();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 'G'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new Green();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 's'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new soy();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 'R'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new Red();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 'b'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new black();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+            if(input.at(0) == 'g'){
+                for(int i = 0; i < input.size(); i++){
+                    Card * tmpCard = new garden();
+                    cardChain.push_back(tmpCard)
+                }
+            }
+        }
+    }
+};
+
+//+= operator overload
+template <class T> Chain<T>& Chain<T>:: operator+=(Card* incomingCard){
+    cardChain.push_back(incomingCard);
+    return *this;
+};
+
+template <class T> int Chain<T>::sell(){
+    if(cardChain.size() == 0) return 0; //if chain is empty. 
+    else return T.getCardsPerCoin(cardChain.size());
+};
+
+//overloading << operator with friend function
+ostream& operator << (ostream& out, Chain<Card>& c){
+    if(c.cardChain.size() == 0) out << " Empty";
+    else{
+        //first print the name of the full name of the bean
+        out << c.cardChain[0]->getName();
+        for(int i = 0; i < c.cardChain.size(); i++){
+            c.cardChain[i]->print(out);
+        }
+    }
+};
+
